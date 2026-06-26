d9f059ec3e547a1525a5f11106ffa14cd7dd937e
MUTHURAJA25 <muthucloud25@gmail.com>
2026-06-26 18:29:02 +0530

    Remove backend configuration for Floci EKS

--- infra/provider.tf
+++ infra/provider.tf
@@ -9,11 +9,11 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    eks       = "http://host.docker.internal:4566"
-    ec2       = "http://host.docker.internal:4566"
-    iam       = "http://host.docker.internal:4566"
-    sts       = "http://host.docker.internal:4566"
-    s3        = "http://host.docker.internal:4566"
-    dynamodb  = "http://host.docker.internal:4566"
+    eks       = "http://localhost:4566"
+    ec2       = "http://localhost:4566"
+    iam       = "http://localhost:4566"
+    sts       = "http://localhost:4566"
+    s3        = "http://localhost:4566"
+    dynamodb  = "http://localhost:4566"
   }
 }
